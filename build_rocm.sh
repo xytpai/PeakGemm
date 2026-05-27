dc5062e567ca097889f274296261cf0d9a94b887
xytpai <xytpai@foxmail.com>
2026-05-27 07:31:42 +0000

    change test

--- build_rocm.sh
+++ build_rocm.sh
@@ -1,1 +1,1 @@
-hipcc -Wno-unused-value -O3 ${@:2} --std=c++20 -Isrc/common -Isrc/impl $1 -o a.out
+hipcc -Wno-unused-value -O3 ${@:2} --std=c++20 -Isrc/common -Isrc/impl -Rpass-analysis=kernel-resource-usage $1 -o a.out
