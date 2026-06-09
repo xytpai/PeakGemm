6f374428ea1b897c9a911a68f7b34ca9c3659c50
Yutao Xu <xytpai@foxmail.com>
2026-06-09 22:30:00 +0800

    Update wmma_utils.h

--- src/common/wmma_utils.h
+++ src/common/wmma_utils.h
@@ -172,12 +172,12 @@ struct WMMA_M16N16K32 {
                 *(floatx4_t *)C,
                 0, 0, 0);
         } else {
-            *(floatx4_t *)D = __builtin_amdgcn_mfma_f32_16x16x32_bf16(
-                (*(bf16x8_t *)A),
-                (*(bf16x8_t *)B),
-                *(floatx4_t *)C,
-                0, 0, 0);
-            // mfma_f32_16x16x32_bf16_inline_acc((*(bf16x8_t *)A), (*(bf16x8_t *)B), *(floatx4_t *)C);
+            // *(floatx4_t *)D = __builtin_amdgcn_mfma_f32_16x16x32_bf16(
+            //     (*(bf16x8_t *)A),
+            //     (*(bf16x8_t *)B),
+            //     *(floatx4_t *)C,
+            //     0, 0, 0);
+            mfma_f32_16x16x32_bf16_inline_acc((*(bf16x8_t *)A), (*(bf16x8_t *)B), *(floatx4_t *)C);
         }
     }
 
